fa04b039e58625dbb662febb9da6401a4e35a0de
Deepak Phulara <deepakphulara7055@gmail.com>
2024-02-15 22:43:08 +0530

    added date and day data in json

--- dailyData.py
+++ dailyData.py
@@ -10,24 +10,28 @@ def getData( browser , url):
         wait = WebDriverWait(browser, 10 )
         print("opened daily data card")
         print("Scrapping data")
-
-
+        
+        date_day_tag = wait.until(EC.presence_of_all_elements_located((By.XPATH, "/html/body/div/div[7]/div[1]/div[1]/div[1]/div")))
         temp_tag = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "temperature")))
         phrase_tag =  wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "phrase")))
         day_night_tag = browser.find_elements(By.CLASS_NAME, "title")
         other_data = browser.find_elements(By.CLASS_NAME, "value")
 
+        date = date_day_tag[0].text
+        date = date.split(",")
+
         dataList = []
         for e in other_data:
             dataList.append(e.text)
-            # print(e.text)
 
         print(len(day_night_tag))
 
         if len(day_night_tag) == 6:  
 
             data = {
-                    "day":{
+                "date":date[1],
+                "day":date[0],
+                    "day_time":{
                             "temp": temp_tag[0].text,
                             "max_uv_index":dataList[0],
                             "wind": dataList[1],
@@ -38,7 +42,7 @@ def getData( browser , url):
                             "cloud_cover": dataList[6],
                             "remark": phrase_tag[0].text,
                             },
-                    "night":{
+                    "night_time":{
                             "temp": temp_tag[1].text,
                             "wind": dataList[7],
                             "wind_gust":dataList[8],
@@ -50,22 +54,21 @@ def getData( browser , url):
                             }
                     }
         else:
-            temp_text = temp_tag.text
-            phrase = phrase_tag.text
-
-            data = {
-                    "day":"data not available",
-                    "night":{
-                            "temp": temp_text,
-                            "wind": dataList[0],
-                            "wind_gust":dataList[1],
-                            "Probability of Precipitation":dataList[2],
-                            "Probability of Thunderstorms":dataList[3],
-                            "Precipitation": dataList[4],
-                            "cloud_cover": dataList[5],
-                            "remark": phrase
-                            }
-                    }
+           data = {
+               "date":date[1],
+                "day":date[0],
+                "day_time":"data not available",
+                "night_time":{
+                        "temp": temp_tag[0].text,
+                        "wind": dataList[0],
+                        "wind_gust":dataList[1],
+                        "Probability of Precipitation":dataList[2],
+                        "Probability of Thunderstorms":dataList[3],
+                        "Precipitation": dataList[4],
+                        "cloud_cover": dataList[5],
+                        "remark": phrase_tag[0].text
+                        }
+                }
 
         print("End of  getData function")
         return data
