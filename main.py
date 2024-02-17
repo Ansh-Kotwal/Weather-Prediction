921d601dcb6ee0dbdd18b89feca8e60aa97ae8b9
ANSH <ansh.kotwal2000@gmail.com>
2024-02-17 11:00:54 +0530

    =Added main(entry) py file

---
+++ main.py
@@ -0,0 +1,13 @@
+import scrapper_v1 
+
+class main:
+    def main():
+     serverRun = scrapper_v1.Scrapping
+
+     location = input("Enter location => ")
+     serverRun.scrappingData(location)
+
+
+# Class run
+test_instance = main
+con = test_instance.main()
