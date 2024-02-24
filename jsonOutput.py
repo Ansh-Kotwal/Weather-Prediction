da31b99b53f4fd0cc9bc74a43df7036202e9f274
ANSH <ansh.kotwal2000@gmail.com>
2024-02-24 19:46:16 +0530

    Added code for exporting json in a seperate folder

--- jsonOutput.py
+++ jsonOutput.py
@@ -1,7 +1,7 @@
 import json
 
 def jsonOutputFile(location , data):
-  with open(f"{location.capitalize()}WeatherInfo.json", "w") as outfile:
+  with open(f"Json/{location.capitalize()}WeatherInfo.json", "w") as outfile:
     json.dump(data, outfile)
   print("Json File Successfully Created")   
   
